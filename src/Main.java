6b36017005416fbd19be664ef4e8c9afe53239a3
Jeongwon Choi <chlwjddnjs17@naver.com>
2023-02-22 00:02:25 +0900

    자바 백준 25314번 / 코딩은 체육과목 입니다.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=a; i++) {
+			if(i%4==0) {
+				System.out.print("long" + " ");
+			}
+			
+		}
+		System.out.print("int");
+	}
+
+}
